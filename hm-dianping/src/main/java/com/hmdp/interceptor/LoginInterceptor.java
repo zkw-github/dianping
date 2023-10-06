57ecfc7be760ce28a098eb1e9aecab1037afea2e
zkw520666 <852187558@qq.com>
2023-10-06 23:15:37 +0800

    Initial commit

---
+++ hm-dianping/src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.hmdp.interceptor;
+
+import com.hmdp.utils.UserHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (UserHolder.getUser() == null) {
+            response.setStatus(401);
+            return false;
+        }
+        return true;
+    }
+
+}
